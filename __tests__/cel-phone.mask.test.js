a49580c7c382659e077cd245a4d8a4b577f63acc
Jeromy Maligie <jmaligie@gmail.com>
2017-09-01 15:22:53 -0700

    ♻️  formatting 🤠

--- __tests__/cel-phone.mask.test.js
+++ __tests__/cel-phone.mask.test.js
@@ -1,125 +1,125 @@
 import { CelPhoneMask } from '../lib/masks';
 
 test('getType results cel-phone', () => {
-    var expected = 'cel-phone';
-    var received = CelPhoneMask.getType();
+  const expected = 'cel-phone';
+  const received = CelPhoneMask.getType();
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('5188888888 results (51) 8888-8888', () => {
-    var mask = new CelPhoneMask();
-    var expected = '(51) 8888-8888';
-    var received = mask.getValue('5188888888');
+  const mask = new CelPhoneMask();
+  const expected = '(51) 8888-8888';
+  const received = mask.getValue('5188888888');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('51888888888 results (51) 88888-8888', () => {
-    var mask = new CelPhoneMask();
-    var expected = '(51) 88888-8888';
-    var received = mask.getValue('51888888888');
+  const mask = new CelPhoneMask();
+  const expected = '(51) 88888-8888';
+  const received = mask.getValue('51888888888');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('88888888 withDDD=false results 8888-8888', () => {
-    var mask = new CelPhoneMask();
-    var expected = '8888-8888';
-    var received = mask.getValue('88888888', {
-        withDDD: false
-    });
+  const mask = new CelPhoneMask();
+  const expected = '8888-8888';
+  const received = mask.getValue('88888888', {
+    withDDD: false,
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('888888888 withDDD=false results 88888-8888', () => {
-    var mask = new CelPhoneMask();
-    var expected = '88888-8888';
-    var received = mask.getValue('888888888', {
-        withDDD: false
-    });
+  const mask = new CelPhoneMask();
+  const expected = '88888-8888';
+  const received = mask.getValue('888888888', {
+    withDDD: false,
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('12377777777 dddMask=999  results 123 7777-7777', () => {
-    var mask = new CelPhoneMask();
-    var expected = '123 7777-7777';
-    var received = mask.getValue('12377777777', {
-        dddMask: '999 '
-    });
+  const mask = new CelPhoneMask();
+  const expected = '123 7777-7777';
+  const received = mask.getValue('12377777777', {
+    dddMask: '999 ',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('123777777777 dddMask=999  results 123 77777-7777', () => {
-    var mask = new CelPhoneMask();
-    var expected = '123 77777-7777';
-    var received = mask.getValue('123777777777', {
-        dddMask: '999 '
-    });
+  const mask = new CelPhoneMask();
+  const expected = '123 77777-7777';
+  const received = mask.getValue('123777777777', {
+    dddMask: '999 ',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('123123 is not valid', () => {
-    var mask = new CelPhoneMask();
-    var expected = false;
-    var received = mask.validate('123123');
+  const mask = new CelPhoneMask();
+  const expected = false;
+  const received = mask.validate('123123');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('5188888888 is valid', () => {
-    var mask = new CelPhoneMask();
-    var isValid = mask.validate('5188888888');
+  const mask = new CelPhoneMask();
+  const isValid = mask.validate('5188888888');
 
-    expect(isValid).toBe(true);
+  expect(isValid).toBe(true);
 });
 
 test('123777777777 dddMask=999  is valid', () => {
-    var mask = new CelPhoneMask();
-    var isValid = mask.validate('123777777777', {
-        dddMask: '999 '
-    });
+  const mask = new CelPhoneMask();
+  const isValid = mask.validate('123777777777', {
+    dddMask: '999 ',
+  });
 
-    expect(isValid).toBe(true);
+  expect(isValid).toBe(true);
 });
 
 test('1237777777 dddMask=999  is not valid', () => {
-    var mask = new CelPhoneMask();
-    var isValid = mask.validate('1237777777', {
-        dddMask: '999 '
-    });
+  const mask = new CelPhoneMask();
+  const isValid = mask.validate('1237777777', {
+    dddMask: '999 ',
+  });
 
-    expect(isValid).toBe(false);
+  expect(isValid).toBe(false);
 });
 
 test('5188888888 results (51) 8888-8888 and raw value 5188888888', () => {
-    var mask = new CelPhoneMask();
-    var expected = '(51) 8888-8888';
-    var received = mask.getValue('5188888888');
+  const mask = new CelPhoneMask();
+  const expected = '(51) 8888-8888';
+  const received = mask.getValue('5188888888');
 
-    var expectedRawValue = '5188888888';
-    var receivedRawValue = mask.getRawValue(received);
+  const expectedRawValue = '5188888888';
+  const receivedRawValue = mask.getRawValue(received);
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
 
 test('123777777777 dddMask=999  results 123 77777-7777 and raw value 123777777777', () => {
-    var mask = new CelPhoneMask();
-    var expected = '123 77777-7777';
-    var received = mask.getValue('123777777777', {
-        dddMask: '999 '
-    });
+  const mask = new CelPhoneMask();
+  const expected = '123 77777-7777';
+  const received = mask.getValue('123777777777', {
+    dddMask: '999 ',
+  });
 
-    var expectedRawValue = '123777777777';
-    var receivedRawValue = mask.getRawValue(received, {
-        dddMask: '999 '
-    });
+  const expectedRawValue = '123777777777';
+  const receivedRawValue = mask.getRawValue(received, {
+    dddMask: '999 ',
+  });
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
--- __tests__/money.mask.test.js
+++ __tests__/money.mask.test.js
@@ -1,198 +1,198 @@
 import { MoneyMask } from '../lib/masks';
 
 test('getType results money', () => {
-    var expected = 'money';
-    var received = MoneyMask.getType();
+  const expected = 'money';
+  const received = MoneyMask.getType();
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1 results R$0,01', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$0,01';
-    var received = mask.getValue('1');
+  const mask = new MoneyMask();
+  const expected = 'R$0,01';
+  const received = mask.getValue('1');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('111 results R$1,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1,11';
-    var received = mask.getValue('111');
+  const mask = new MoneyMask();
+  const expected = 'R$1,11';
+  const received = mask.getValue('111');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1111 results R$11,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$11,11';
-    var received = mask.getValue('1111');
+  const mask = new MoneyMask();
+  const expected = 'R$11,11';
+  const received = mask.getValue('1111');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('11111 results R$111,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$111,11';
-    var received = mask.getValue('11111');
+  const mask = new MoneyMask();
+  const expected = 'R$111,11';
+  const received = mask.getValue('11111');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('111111 results R$1.111,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1.111,11';
-    var received = mask.getValue('111111');
+  const mask = new MoneyMask();
+  const expected = 'R$1.111,11';
+  const received = mask.getValue('111111');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('111111111 results R$1.111.111,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1.111.111,11';
-    var received = mask.getValue('111111111');
+  const mask = new MoneyMask();
+  const expected = 'R$1.111.111,11';
+  const received = mask.getValue('111111111');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test(' results R$0,00', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$0,00';
-    var received = mask.getValue('');
+  const mask = new MoneyMask();
+  const expected = 'R$0,00';
+  const received = mask.getValue('');
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('11111 precision 3 results R$11,111', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$11,111';
-    var received = mask.getValue('11111', {
-        precision: 3
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$11,111';
+  const received = mask.getValue('11111', {
+    precision: 3,
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('111 separator . results R$1.11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1.11';
-    var received = mask.getValue('111', {
-        separator: '.'
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$1.11';
+  const received = mask.getValue('111', {
+    separator: '.',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('111111 delimiter , results R$1,111,11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1,111,11';
-    var received = mask.getValue('111111', {
-        delimiter: ','
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$1,111,11';
+  const received = mask.getValue('111111', {
+    delimiter: ',',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1 unit US$ results US$0,01', () => {
-    var mask = new MoneyMask();
-    var expected = 'US$0,01';
-    var received = mask.getValue('1', {
-        unit: 'US$'
-    });
+  const mask = new MoneyMask();
+  const expected = 'US$0,01';
+  const received = mask.getValue('1', {
+    unit: 'US$',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1 suffixUnit $$$ results R$0,01', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$0,01 $$$';
-    var received = mask.getValue('1', {
-        suffixUnit: '$$$'
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$0,01 $$$';
+  const received = mask.getValue('1', {
+    suffixUnit: '$$$',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1 zeroCents results R$1,00', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1,00';
-    var received = mask.getValue('1', {
-        zeroCents: true
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$1,00';
+  const received = mask.getValue('1', {
+    zeroCents: true,
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('US$ config with value 1234567 results US$12,345.67', () => {
-    var mask = new MoneyMask();
-    var expected = 'US$12,345.67';
-    var received = mask.getValue('1234567', {
-        unit: 'US$',
-        delimiter: ',',
-        separator: '.'
-    });
+  const mask = new MoneyMask();
+  const expected = 'US$12,345.67';
+  const received = mask.getValue('1234567', {
+    unit: 'US$',
+    delimiter: ',',
+    separator: '.',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
 
 test('1 results R$0,01 and raw value 0.01', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$0,01';
-    var received = mask.getValue('1');
+  const mask = new MoneyMask();
+  const expected = 'R$0,01';
+  const received = mask.getValue('1');
 
-    var expectedRawValue = 0.01;
-    var receivedRawValue = mask.getRawValue(received);
+  const expectedRawValue = 0.01;
+  const receivedRawValue = mask.getRawValue(received);
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
 
 test('111111 results R$1.111,11 and raw value 1111.11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1.111,11';
-    var received = mask.getValue('111111');
+  const mask = new MoneyMask();
+  const expected = 'R$1.111,11';
+  const received = mask.getValue('111111');
 
-    var expectedRawValue = 1111.11;
-    var receivedRawValue = mask.getRawValue(received);
+  const expectedRawValue = 1111.11;
+  const receivedRawValue = mask.getRawValue(received);
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
 
 test('1 zeroCents results R$1,00 and raw value 1', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1,00';
-    var received = mask.getValue('1', {
-        zeroCents: true
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$1,00';
+  const received = mask.getValue('1', {
+    zeroCents: true,
+  });
 
-    var expectedRawValue = 1;
-    var receivedRawValue = mask.getRawValue(received);
+  const expectedRawValue = 1;
+  const receivedRawValue = mask.getRawValue(received);
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
 
 test('111111 delimiter , results R$1,111,11 and raw value 1111.11', () => {
-    var mask = new MoneyMask();
-    var expected = 'R$1,111,11';
-    var received = mask.getValue('111111', {
-        delimiter: ','
-    });
+  const mask = new MoneyMask();
+  const expected = 'R$1,111,11';
+  const received = mask.getValue('111111', {
+    delimiter: ',',
+  });
 
-    var expectedRawValue = 1111.11;
-    var receivedRawValue = mask.getRawValue(received);
+  const expectedRawValue = 1111.11;
+  const receivedRawValue = mask.getRawValue(received);
 
-    expect(received).toBe(expected);
-    expect(receivedRawValue).toBe(expectedRawValue);
+  expect(received).toBe(expected);
+  expect(receivedRawValue).toBe(expectedRawValue);
 });
 
 test('1 unit US$  results US$ 0,01', () => {
-    var mask = new MoneyMask();
-    var expected = 'US$ 0,01';
-    var received = mask.getValue('1', {
-        unit: 'US$ '
-    });
+  const mask = new MoneyMask();
+  const expected = 'US$ 0,01';
+  const received = mask.getValue('1', {
+    unit: 'US$ ',
+  });
 
-    expect(received).toBe(expected);
+  expect(received).toBe(expected);
 });
