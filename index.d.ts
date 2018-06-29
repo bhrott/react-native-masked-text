51697741c71196e50f84be1d21aff4ee126674fe
enagorny <ideviantik@gmail.com>
2018-06-29 18:19:03 +0300

    TypeScript support for custom mask options

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {TextInput, TextInputProps} from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 
 // Type prop of TextInputMask.
 type TextInputMaskTypeProp = "credit-card" | "cpf" | "cnpj" | "zip-code" | "only-numbers" | "money" | "cel-phone" | "datetime" | "custom";
@@ -23,6 +23,12 @@ type TextInputMaskOptionProp = {
 
   // Credit card type.
   obfuscated?: boolean;
+
+  // Custom type.
+  mask?: string;
+  validator?: (value: string, settings: TextInputMaskOptionProp) => boolean;
+  getRawValue?: (value: string, settings: TextInputMaskOptionProp) => boolean;
+  translation?: { [s: string]: (val: string) => string | null | undefined; };
 };
 
 // TextInputMask Props
@@ -34,7 +40,7 @@ interface TextInputMaskProps extends TextInputProps {
 }
 
 // TextInputMask Component
-export declare class TextInputMask extends React.Component<TextInputMaskProps> {}
+export declare class TextInputMask extends React.Component<TextInputMaskProps> { }
 
 // MaskService
 export declare class MaskService {
